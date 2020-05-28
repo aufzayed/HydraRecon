3f6e1852c0578489fef91bcfd2e9c0066347c3cb
aufzayed <40190200+aufzayed@users.noreply.github.com>
2020-05-28 21:55:08 +0200

    Bug Fixes

--- core/basic/helpers.py
+++ core/basic/helpers.py
@@ -11,21 +11,27 @@ def relative_to_absolute(file, domain):
 
     for script in scripts:
         if script.get('src'):
-            if script['src'][0:8] == 'http://' or script['src'][0:9] == 'https://':
+            if script['src'][0:7] == 'http://' or script['src'][0:8] == 'https://':
                 pass
+            elif script['src'][0:7] == 'http://' or script['src'][0:8] == 'https://' or script['src'][0] != '.' or script['src'][0] != '/':
+                script['src'] = f'{domain}/{script}'
             elif script['src'][0:2] == '//':
                 script['src'] = f'http:{script["src"]}'
             elif script['src'][0] == '/' and script['src'][1] != '/':
                 script['src'] = f'{domain}{script["src"]}'
-            elif script['src'][0] != '/' and script['src'][0] != '.':
+            elif script['src'][0:2] == './':
+                script['src'] = f'{domain}/{script["src"]}'
+            elif script['src'][0:3] == '../':
                 script['src'] = f'{domain}/{script["src"]}'
         else:
             pass
 
     for link in links:
         if link.get('href'):
-            if link['href'][0:2] == '//' or link['href'][0:8] == 'http://' or link['href'][0:9] == 'https://':
+            if link['href'][0:2] == '//' or link['href'][0:7] == 'http://' or link['href'][0:8] == 'https://':
                 pass
+            if link['href'][0:2] != '//' or link['href'][0:7] != 'http://' or link['href'][0:8] != 'https://' or link['href'][0] != '/' or link['href'][0] != '.':
+                link['href'] = f'{domain}/{link["href"]}'
             elif link['href'][0] == '/' and link['src'][1] != '/':
                 link['href'] = f'{domain}{link["href"]}'
             elif link['href'][0] == '.':
@@ -33,8 +39,10 @@ def relative_to_absolute(file, domain):
 
     for img in imgs:
         if img.get('src'):
-            if img['src'][0:2] == '//' or img['src'][0:8] == 'http://' or img['src'][0:9] == 'https://' or img['src'][0:6] == 'data:':
+            if img['src'][0:2] == '//' or img['src'][0:7] == 'http://' or img['src'][0:8] == 'https://' or img['src'][0:5] == 'data:':
                 pass
+            if img['src'][0:2] != '//' or img['src'][0:7] != 'http://' or img['src'][0:8] != 'https://' or img['src'][0:5] != 'data:' or img['src'][0] != '/' or img['src'][0] != '.':
+                img['src'] = f'{domain}/{img["src"]}'
             elif img['src'][0] == '/' and img['src'][1] != '/':
                 img['src'] = f'{domain}{img["src"]}'
             elif img['src'][0] == '.':
